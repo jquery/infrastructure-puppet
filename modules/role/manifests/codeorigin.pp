ef670ae29d33295b882d25f140b4ce0a9d98a5d5
Ori Livneh <ori.livneh@gmail.com>
2022-06-01 23:24:42 -0400

    Add codeorigin nginx config
    Branched from /cfg/default.conf in jquery/codeorigin.jquery.com@63b369c.

--- modules/role/manifests/codeorigin.pp
+++ modules/role/manifests/codeorigin.pp
@@ -3,4 +3,9 @@
 class role::codeorigin {
   include jquery::base
   include nginx
+
+  nginx::site { 'codeorigin':
+    source  => 'puppet:///modules/role/codeorigin.nginx',
+    notify  => Exec['nginx-reload'],
+  }
 }
