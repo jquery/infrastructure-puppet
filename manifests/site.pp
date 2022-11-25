90725fff852eefb4d1eaab00f962626c6b2a8d50
Taavi Väänänen <hi@taavi.wtf>
2022-11-25 21:40:08 +0200

    site: add roles for new servers

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,10 @@
 # https://puppet.com/docs/puppet/7/lang_node_definitions.html
 # https://puppet.com/docs/puppet/7/roles_and_profiles_example.html
 
-node 'codeorigin-01.stage.ops.jquery.net' {
-    include role::codeorigin
+node 'codeorigin-02.stage.ops.jquery.net' {
+  include role::codeorigin
+}
+
+node 'puppet-03.stage.ops.jquery.net' {
+  include role::puppet
 }
---
+++ modules/role/manifests/puppet.pp
@@ -0,0 +1,4 @@
+# @summary a puppet server
+class role::puppet () {
+  include jquery::base
+}
