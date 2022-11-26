ea12ac989343a991eba77c509ac3e92b33ccdbdf
Taavi Väänänen <hi@taavi.wtf>
2022-11-26 16:11:57 +0200

    P:codeorigin: try to get this to work

--- modules/profile/manifests/codeorigin.pp
+++ modules/profile/manifests/codeorigin.pp
@@ -1,5 +1,16 @@
 # @summary code origin server
-class profile::codeorigin () {
+# @summary $cdn_access_key cdn access key
+class profile::codeorigin (
+  String $cdn_access_key = lookup('profile::codeorigin::cdn_access_key'),
+) {
+  git::clone { 'codeorigin':
+    path   => '/srv/codeorigin',
+    remote => 'https://github.com/jquery/codeorigin.jquery.com',
+    branch => 'main',
+    owner  => 'root',
+    group  => 'www-data',
+  }
+
   nginx::site { 'codeorigin':
     content => template('profile/codeorigin/site.nginx.erb'),
   }
