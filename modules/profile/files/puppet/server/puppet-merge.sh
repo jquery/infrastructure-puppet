be26963b0da1753b235778a3b47766b14f8a0de3
Taavi Väänänen <hi@taavi.wtf>
2022-11-26 11:46:32 +0200

    P:puppet::server: use spaces in puppet-merge.sh

--- modules/profile/files/puppet/server/puppet-merge.sh
+++ modules/profile/files/puppet/server/puppet-merge.sh
@@ -12,16 +12,16 @@ git fetch origin -v
 PAGER="" git diff "$GIT_BRANCH"..origin/"$GIT_BRANCH"
 
 while true; do
-	read -rp " --- Approve the changes (y/n)? " choice
-	case "$choice" in
-		y|Y)
-			break
-			;;
-		n|N)
-			echo "Aborted!"
-			exit 1
-			;;
-	esac
+    read -rp " --- Approve the changes (y/n)? " choice
+    case "$choice" in
+        y|Y)
+            break
+            ;;
+        n|N)
+            echo "Aborted!"
+            exit 1
+            ;;
+    esac
 done
 
 ORIGINAL_PUPPETFILE_HASH=$(sha512sum Puppetfile)
@@ -32,6 +32,6 @@ git merge --ff-only origin/"$GIT_BRANCH"
 NEW_PUPPETFILE_HASH=$(sha512sum Puppetfile)
 
 if [ "$ORIGINAL_PUPPETFILE_HASH" != "$NEW_PUPPETFILE_HASH" ]; then
-	echo " -- Updating external modules"
-  g10k -puppetfile
+    echo " -- Updating external modules"
+    g10k -puppetfile
 fi
