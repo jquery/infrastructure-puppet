c7131236b1779ae9ce73dc3ac9313f4cf3a8cbb1
Taavi Väänänen <hi@taavi.wtf>
2022-11-26 18:18:54 +0200

    letsencrypt: add a define to request a new cert

---
+++ modules/letsencrypt/manifests/certificate.pp
@@ -0,0 +1,19 @@
+# @summary generates and maintains a certificate with the webroot hook
+# @param $domains certificate subjects
+define letsencrypt::certificate (
+  Array[Stdlib::Fqdn] $domains,
+) {
+  require letsencrypt::certbot
+
+  $base_command = "/usr/bin/certbot certonly --cert-name ${title} --webroot --webroot-path /var/www/letsencrypt --non-interactive"
+  $domains_command = $domains.map |Stdlib::Fqdn $domain| {
+    "-d ${domain}"
+  }.join(' ')
+
+  # TODO: replace the cert if the domains change
+  exec { "letsencrypt-request-${title}":
+    command => "${base_command} ${domains_command}",
+    creates => "/etc/letsencrypt/live/${title}",
+    logoutput => true,
+  }
+}
