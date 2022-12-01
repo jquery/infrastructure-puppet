71787f42e20b5a81a37badde2aefa26b4f2bdda1
Taavi Väänänen <hi@taavi.wtf>
2022-12-01 08:39:28 +0200

    site: add puppet-03.ops

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,14 @@
 # https://puppet.com/docs/puppet/7/lang_node_definitions.html
 # https://puppet.com/docs/puppet/7/roles_and_profiles_example.html
 
+# Production
+
+node 'puppet-03.ops.jquery.net' {
+  role('puppet')
+}
+
+# Staging
+
 node 'codeorigin-02.stage.ops.jquery.net' {
   role('codeorigin')
 }
