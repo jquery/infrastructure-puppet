ed9aaf827869a328deca567b9c95b51c481f70e6
Taavi Väänänen <hi@taavi.wtf>
2022-12-01 16:39:35 +0200

    config-version: pass the branch name

--- bin/config-version.sh
+++ bin/config-version.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# $1 = environment_name (passed by puppet, ununused)
 set -euo pipefail
 
 # Ignore local install paths
@@ -12,7 +11,7 @@ script_dir=$(dirname "$0")
 for repo_dir in "${script_dir}/../.git" /tmp/g10k/environments/jquery-puppet.git/
 do
   if [ -d "$repo_dir" ]; then
-    git --git-dir "${repo_dir}" log -1 --pretty='(%h) %cn - %s'
+    git --git-dir "${repo_dir}" log -1 --pretty='(%h) %cn - %s' "$1"
     exit 0
   fi
 done
