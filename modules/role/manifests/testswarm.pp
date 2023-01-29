31908944d01cab6c253dd9c54979ab5645ad4bf6
Taavi Väänänen <hi@taavi.wtf>
2023-01-29 20:38:51 +0200

    P:testswarm::server: set up notifier for code updates

--- modules/role/manifests/testswarm.pp
+++ modules/role/manifests/testswarm.pp
@@ -2,6 +2,8 @@
 class role::testswarm {
   include profile::base
   include profile::certbot
+  include profile::notifier
+
   include profile::testswarm::database
   include profile::testswarm::server
 }
