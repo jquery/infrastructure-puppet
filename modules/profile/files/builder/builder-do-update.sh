f87863b0dc72ff29dd566ec8e69cb6cc7cc59384
Taavi Väänänen <hi@taavi.wtf>
2023-03-24 14:37:16 +0200

    P:builder: run npm update
    Since we don't use `npm ci` we need to do this.

--- modules/profile/files/builder/builder-do-update.sh
+++ modules/profile/files/builder/builder-do-update.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cd "$1"
 npm install --cache /tmp/npm-cache-builder
+npm update --cache /tmp/npm-cache-builder
+npm prune --cache /tmp/npm-cache-builder
 node_modules/.bin/grunt --no-color deploy
