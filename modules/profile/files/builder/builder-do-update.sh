bc32beffa59c339e2bf6c83e713559b1ccb862f8
Taavi Väänänen <hi@taavi.wtf>
2023-07-07 20:39:43 +0300

    P:builder: cleanup files after update
    Otherwise the notifier hook will fail with unstaged changes.

--- modules/profile/files/builder/builder-do-update.sh
+++ modules/profile/files/builder/builder-do-update.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+set -euxo pipefail
+
 cd "$1"
 npm install --cache /tmp/npm-cache-builder
 npm update --cache /tmp/npm-cache-builder
 npm prune --cache /tmp/npm-cache-builder
 node_modules/.bin/grunt --no-color deploy
+
+# Reset to a clean state so that the next deploy will succeed.
+git clean -d --force
+git reset --hard
