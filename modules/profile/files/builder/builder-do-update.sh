bbf115584120fe08fa768c41f1a4bed9ae23be63
Taavi Väänänen <hi@taavi.wtf>
2023-07-09 13:17:18 +0300

    P:builder: use trap to ensure cleanup on failure

--- modules/profile/files/builder/builder-do-update.sh
+++ modules/profile/files/builder/builder-do-update.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 
+cleanup() {
+  # Reset to a clean state so that the next deploy (and its git update) will succeed.
+  git clean -d --force
+  git reset --hard
+}
+trap cleanup EXIT
+
 export NODE_ENV=production
 
 cd "$1"
@@ -8,7 +15,3 @@ npm install
 npm update
 npm prune
 node_modules/.bin/grunt --no-color deploy
-
-# Reset to a clean state so that the next deploy will succeed.
-git clean -d --force
-git reset --hard
