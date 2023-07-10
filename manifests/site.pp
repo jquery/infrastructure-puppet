6b6540fa73160419467edfce325e01673fc05eac
Timo Tijhof <krinkle@fastmail.com>
2023-07-10 02:47:35 +0100

    site: add filestash-01, wp-04, wp-05; document prod server specs

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,40 +3,62 @@
 # https://puppet.com/docs/puppet/7/roles_and_profiles_example.html
 
 # Production
+# ----------
 
+# 2 CPU, 2 GB mem
 node 'codeorigin-02.ops.jquery.net' {
   role('codeorigin')
 }
 
+# 2 CPU, 4 GB mem, 80 GB disk
 node 'contentorigin-02.ops.jquery.net' {
   role('contentorigin')
 }
 
+# 1 CPU, 2 GB mem
 node 'gruntjs-02.ops.jquery.net' {
   role('gruntjscom')
 }
 
+# 2 CPU, 4 GB mem
 node 'puppet-03.ops.jquery.net' {
   role('puppet')
 }
 
+# 2 CPU, 4 GB mem, 80 GB disk
 node 'swarm-02.ops.jquery.net' {
   role('testswarm')
 }
 
+# 2 CPU, 2 GB mem
 node 'miscweb-01.ops.jquery.net' {
   role('miscweb')
 }
 
+# 1 CPU, 2 GB mem
 node 'wpblogs-01.ops.jquery.net' {
   role('blogs')
 }
 
+# 2 CPU, 2 GB mem
 node 'search-02.ops.jquery.net' {
   role('search')
 }
 
+# 2 CPU, 2 GB mem
+node 'filestash-01.ops.jquery.net' {
+}
+
+# 4 CPU, 8 GB mem (NYC3)
+node 'wp-04.ops.jquery.net' {
+}
+
+# 4 CPU, 8 GB mem (SFO3)
+node 'wp-05.ops.jquery.net' {
+}
+
 # Staging
+# -------
 
 node 'builder-04.stage.ops.jquery.net' {
   role('docs::builder')
