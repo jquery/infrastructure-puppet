7f674fa6a338b7f756848b2e8be370fd36398719
Taavi Väänänen <hi@taavi.wtf>
2023-07-21 22:54:35 +0300

    P:filestash: add deploy ssh key support

--- modules/profile/manifests/filestash.pp
+++ modules/profile/manifests/filestash.pp
@@ -1,17 +1,30 @@
 # @summary file stash for constantly updating release files
 class profile::filestash (
-  String[1] $certificate = lookup('profile::filestash::certificate'),
+  String[1]             $certificate = lookup('profile::filestash::certificate'),
+  Array[Users::Ssh_key] $deploy_keys = lookup('profile::filestash::deploy_keys', {default_value => []}),
 ) {
   systemd::sysuser { 'filestash':
     content => 'u filestash - "unprivileged user for updating filestash files" /srv/filestash',
   }
 
-  file { '/srv/filestash':
+  file { [
+    '/srv/filestash',
+    '/srv/filestash/data'
+  ]:
     ensure  => directory,
     owner   => 'filestash',
     group   => 'filestash',
     mode    => '0775',
     require => Systemd::Sysuser['filestash'],
+  }
+
+  $deploy_keys.each |Integer $i, Users::Ssh_key $key| {
+    ssh_authorized_key { "filestash_${i}":
+      user    => $title,
+      type    => $key['type'],
+      key     => $key['key'],
+      require => Systemd::Sysuser['filestash'],
+    }
   }
 
   $tls_config = nginx::tls_config()
