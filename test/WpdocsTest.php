4f152f8e50cd08fc603eeaf5172a6424b38630f8
Timo Tijhof <krinkle@fastmail.com>
2023-08-26 03:48:22 +0100

    P:wordpress::docs: add jquerymobile tests, mark both /blog redirects permanent

--- test/WpdocsTest.php
+++ test/WpdocsTest.php
@@ -9,6 +9,16 @@ $server = @$argv[1] ?: '';
 Unit::start();
 
 foreach ( [
+  'https://jquery.com/blog/feed/' => 'https://blog.jquery.com/feed/',
+  'https://jquery.com/blog/2008/09/28/jquery-microsoft-nokia' => 'https://blog.jquery.com/2008/09/28/jquery-microsoft-nokia',
+] as $url => $expected ) {
+  Unit::testHttp( $url, null, [], [
+    'status' => '301',
+    'location' => $expected,
+  ] );
+}
+
+foreach ( [
   'https://jqueryui.com/bugs/ticket/3484' => 'http://bugs.jqueryui.com/ticket/3484',
 ] as $url => $expected ) {
   Unit::testHttp( $url, null, [], [
@@ -18,11 +28,11 @@ foreach ( [
 }
 
 foreach ( [
-  'https://jquery.com/blog/feed/' => 'https://blog.jquery.com/feed/',
-  'https://jquery.com/blog/2008/09/28/jquery-microsoft-nokia' => 'https://blog.jquery.com/2008/09/28/jquery-microsoft-nokia',
+  'https://jquerymobile.com/blog/feed/' => 'https://blog.jquerymobile.com/feed/',
+  'https://jquerymobile.com/blog/2011/11/16/announcing-jquery-mobile-1-0/' => 'https://blog.jquerymobile.com/2011/11/16/announcing-jquery-mobile-1-0/',
 ] as $url => $expected ) {
   Unit::testHttp( $url, null, [], [
-    'status' => '302',
+    'status' => '301',
     'location' => $expected,
   ] );
 }
