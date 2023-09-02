3c7a8f77e0aab4eb0ff50663aba588d7d4b099df
Taavi Väänänen <hi@taavi.wtf>
2023-09-02 15:17:10 +0300

    P:wordpress: add default-tls site
    Right now hitting port 443 on a wordpress node without a hostname will
    serve the first site. Add a catch-all site that just returns errors to
    hopefully reduce PHP load on the server.

--- modules/profile/manifests/wordpress/base.pp
+++ modules/profile/manifests/wordpress/base.pp
@@ -1,7 +1,8 @@
 # @summary common config for any wordpress site
 class profile::wordpress::base (
-  String[1] $innodb_buffer_pool_size = lookup('profile::wordpress::base::mariadb_innodb_buffer_pool_size', {default_value => '512M'}),
-  String[1] $wordpress_cli_version   = lookup('profile::wordpress::base::wordpress_cli_version'),
+  String[1]           $innodb_buffer_pool_size = lookup('profile::wordpress::base::mariadb_innodb_buffer_pool_size', {default_value => '512M'}),
+  String[1]           $wordpress_cli_version   = lookup('profile::wordpress::base::wordpress_cli_version'),
+  Optional[String[1]] $default_site_cert       = lookup('profile::wordpress::base::default_site_cert', {default_value => undef}),
 ) {
   file { '/srv/mariadb':
     ensure => directory,
@@ -43,5 +44,15 @@ class profile::wordpress::base (
 
   class { 'wordpress::cli':
     version => $wordpress_cli_version,
+  }
+
+  # Provision a default site on port 443, so that all the crawlers hitting
+  # the HTTPS port without a real host name don't end up hitting WordPress/PHP
+  # and consuming resources.
+  if $default_site_cert {
+    $tls_config = nginx::tls_config()
+    nginx::site { 'default-tls':
+      content => template('profile/wordpress/base/default-tls.nginx.erb'),
+    }
   }
 }
