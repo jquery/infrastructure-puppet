b9fd16cfa130131f13b42cb0875c19c8987640d8
Timo Tijhof <krinkle@fastmail.com>
2023-09-18 21:41:50 -0700

    wordpress,tests: sync more permanent vs non-permanent status

--- test/WpdocsTest.php
+++ test/WpdocsTest.php
@@ -15,18 +15,23 @@ foreach ( [
   'https://jquery.com/dev/bugs/new/' => 'https://bugs.jquery.com',
   'https://jquery.com/dev/svn/trunk/plugins/' => 'https://bugs.jquery.com',
   'https://jquery.com/docs/Base/Expression/' => 'https://api.jquery.com',
+  'https://jquery.com/latest' => 'https://code.jquery.com',
   'https://jquery.com/blog/2008/09/28/jquery-microsoft-nokia' => 'https://blog.jquery.com/2008/09/28/jquery-microsoft-nokia',
 
+  'https://jqueryui.com/latest/ui/jquery.effects.core.js' => 'https://code.jquery.com',
   'https://jqueryui.com/docs/Changelog/1.13.0' => 'https://jqueryui.com/changelog/1.13.0',
   'https://jqueryui.com/docs/Theming' => 'https://learn.jquery.com/jquery-ui/theming/',
   'https://jqueryui.com/docs/About' => 'https://jqueryui.com/About',
   'https://jqueryui.com/docs/dialog' => 'https://jqueryui.com/dialog',
+  'https://jqueryui.com/bugs/ticket/3484' => 'https://bugs.jqueryui.com/ticket/3484',
 
   'https://jquerymobile.com/blog/feed' => 'https://blog.jquerymobile.com/feed',
   'https://jquerymobile.com/blog/feed/' => 'https://blog.jquerymobile.com/feed/',
   'https://jquerymobile.com/blog/2011/11/16/announcing-jquery-mobile-1-0/' => 'https://blog.jquerymobile.com/2011/11/16/announcing-jquery-mobile-1-0/',
 
   'https://jquery.org/feed/' => 'https://meetings.jquery.org/feed/',
+
+  'https://contribute.jquery.org/CLA/status/' => 'https://cla.openjsf.org',
 
   'https://releases.jquery.com/jquery' => 'https://releases.jquery.com/jquery/',
   'https://releases.jquery.com/ui' => 'https://releases.jquery.com/ui/',
@@ -45,7 +50,6 @@ foreach ( [
   'http://jquery.com/discuss/' => 'https://forum.jquery.com/',
   'http://jquery.com/discuss/2006-May/000804/' => 'https://www.mail-archive.com/discuss@jquery.com/',
   'http://jquery.com/forum' => 'https://forum.jquery.com',
-  'http://jquery.com/latest' => 'https://code.jquery.com',
   'http://jquery.com/plugins' => 'https://plugins.jquery.com',
   'http://jquery.com/src' => 'https://code.jquery.com',
   'http://jquery.com/ui' => 'https://jqueryui.com',
@@ -54,16 +58,11 @@ foreach ( [
   'http://api.jquery.com/api' => 'https://api.jquery.com/resources/api.xml',
   'http://api.jquery.com/api/foo' => 'https://api.jquery.com/resources/api.xml',
 
-  'http://jqueryui.com/latest/ui/jquery.effects.core.js' => 'https://code.jquery.com',
-  'https://jqueryui.com/bugs/ticket/3484' => 'https://bugs.jqueryui.com/ticket/3484',
-
   'https://api.jqueryui.com/api' => 'https://api.jqueryui.com/resources/api.xml',
   'https://api.jqueryui.com/api/foo' => 'https://api.jqueryui.com/resources/api.xml',
 
   'https://api.jquerymobile.com/api' => 'https://api.jquerymobile.com/resources/api.xml',
   'https://api.jquerymobile.com/api/foo' => 'https://api.jquerymobile.com/resources/api.xml',
-
-  'https://contribute.jquery.org/CLA/status/' => 'https://cla.openjsf.org',
 
 ] as $url => $expected ) {
   Unit::testHttp( $url, null, [], [
