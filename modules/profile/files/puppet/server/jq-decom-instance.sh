4e2f79b79a4cc837b32747497e991ff4ae9ecf48
Taavi Väänänen <hi@taavi.wtf>
2023-09-29 21:33:53 +0300

    P:puppet: fix typo

--- modules/profile/files/puppet/server/jq-decom-instance.sh
+++ modules/profile/files/puppet/server/jq-decom-instance.sh
@@ -6,20 +6,20 @@ set -euo pipefail
 
 INSTANCE="$1"
 
-TARNSAP_KEYFILE="$TARSNAP_KEYS_BASE_PATH/$INSTANCE.key"
+TARSNAP_KEYFILE="$TARSNAP_KEYS_BASE_PATH/$INSTANCE.key"
 
 echo "=== Cleaning up $INSTANCE"
 
-if [ -f "$TARNSAP_KEYFILE" ]; then
+if [ -f "$TARSNAP_KEYFILE" ]; then
   echo "Cleaning up old Tarsnap backups and key file"
   if ! git -C "$TARSNAP_KEYS_BASE_PATH" diff-index --quiet HEAD --; then
     echo "The private git directory has uncommitted changes, please fix that before running this script."
     exit 1
   fi
 
-  tarsnap --keyfile "$TARNSAP_KEYFILE" --nuke
+  tarsnap --keyfile "$TARSNAP_KEYFILE" --nuke
 
-  git -C "$TARSNAP_KEYS_BASE_PATH" rm "$TARNSAP_KEYFILE"
+  git -C "$TARSNAP_KEYS_BASE_PATH" rm "$TARSNAP_KEYFILE"
   git -C "$TARSNAP_KEYS_BASE_PATH" commit -m "remove Tarsnap keys for $INSTANCE"
 fi
 
