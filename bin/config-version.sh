a6ea809457ee7eafec2c7c6b79c78ab7988d7b39
Taavi Väänänen <hi@taavi.wtf>
2023-10-27 20:43:20 +0300

    Fix Puppet config version reporting
    The previous commit fixes the issue with octocatalog-diff.
    
    Closes: https://github.com/jquery/infrastructure-puppet/issues/27

--- bin/config-version.sh
+++ bin/config-version.sh
@@ -11,7 +11,7 @@ script_dir=$(dirname "$0")
 for repo_dir in "${script_dir}/../.git" /tmp/g10k/environments/jquery-puppet.git/
 do
   if [ -d "$repo_dir" ]; then
-    git --git-dir "${repo_dir}" log -1 --pretty='(%h) %cn - %s' -- "$1"
+    git --git-dir "${repo_dir}" log -1 --pretty='(%h) %cn - %s' "$1"
     exit 0
   fi
 done
