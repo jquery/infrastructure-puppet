953b247cbc07f07cec58d9ad6516279a3791701f
Timo Tijhof <krinkle@fastmail.com>
2023-11-12 19:16:53 +0000

    site: document OS versions

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,62 +7,62 @@
 # Production
 # ----------
 
-# 2 CPU, 2 GB mem
+# 2 CPU, 2 GB mem, Debian 12 Bookworm
 node 'builder-02.ops.jquery.net' {
   role('docs::builder')
 }
 
-# 2 CPU, 2 GB mem
+# 2 CPU, 2 GB mem, Debian 11 Bullseye
 node 'codeorigin-02.ops.jquery.net' {
   role('codeorigin')
 }
 
-# 2 CPU, 4 GB mem, 80 GB disk
+# 2 CPU, 4 GB mem, Debian 11 Bullseye, 80 GB disk
 node 'contentorigin-02.ops.jquery.net' {
   role('contentorigin')
 }
 
-# 2 CPU, 2 GB mem
+# 2 CPU, 2 GB mem, Debian 12 Bookworm
 node 'filestash-01.ops.jquery.net' {
   role('docs::filestash')
 }
 
-# 1 CPU, 2 GB mem
+# 1 CPU, 2 GB mem, Debian 11 Bullseye
 node 'gruntjs-02.ops.jquery.net' {
   role('gruntjscom')
 }
 
-# 2 CPU, 2 GB mem
+# 2 CPU, 2 GB mem, Debian 11 Bullseye
 node 'miscweb-01.ops.jquery.net' {
   role('miscweb')
 }
 
-# 2 CPU, 4 GB mem
+# 2 CPU, 4 GB mem, Debian 11 Bullseye
 node 'puppet-03.ops.jquery.net' {
   role('puppet')
 }
 
-# 2 CPU, 4 GB mem, 80 GB disk
+# 2 CPU, 4 GB mem, Debian 11 Bullseye, 80 GB disk
 node 'swarm-02.ops.jquery.net' {
   role('testswarm')
 }
 
-# 2 CPU, 2 GB mem
+# 2 CPU, 2 GB mem, Debian 11 Bullseye
 node 'search-02.ops.jquery.net' {
   role('search')
 }
 
-# 4 CPU, 8 GB mem (NYC3)
+# 4 CPU, 8 GB mem, Debian 12 Bookworm (NYC3)
 node 'wp-04.ops.jquery.net' {
   role('docs::wordpress')
 }
 
-# 4 CPU, 8 GB mem (SFO3)
+# 4 CPU, 8 GB mem, Debian 12 Bookworm (SFO3)
 node 'wp-05.ops.jquery.net' {
   role('docs::wordpress')
 }
 
-# 1 CPU, 2 GB mem
+# 1 CPU, 2 GB mem, Debian 11 Bullseye
 node 'wpblogs-01.ops.jquery.net' {
   role('blogs')
 }
@@ -70,18 +70,22 @@ node 'wpblogs-01.ops.jquery.net' {
 # Staging
 # -------
 
+# Debian 11 Bullseye
 node 'builder-04.stage.ops.jquery.net' {
   role('docs::builder')
 }
 
+# Debian 11 Bullseye
 node 'codeorigin-02.stage.ops.jquery.net' {
   role('codeorigin')
 }
 
+# Debian 11 Bullseye
 node 'gruntjs-02.stage.ops.jquery.net' {
   role('gruntjscom')
 }
 
+# Debian 12 Bookworm
 node 'wp-03.stage.ops.jquery.net' {
   role('docs::wordpress')
 }
