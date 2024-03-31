e24cafc9694671036e935fdf562ee632508aa80a
Timo Tijhof <krinkle@fastmail.com>
2024-03-30 18:16:57 -0700

    bin: Hotpatch octocatalog-diff to not crash on Ruby 3.1.2
    Ref https://github.com/github/octocatalog-diff/issues/261

--- bin/octodiff-docker.sh
+++ bin/octodiff-docker.sh
@@ -10,6 +10,7 @@ apt-get update -qq && \
 DEBIAN_FRONTEND=noninteractive DEBCONF_NOWARNINGS=yes apt-get install -y -qq git octocatalog-diff puppet-agent puppet-module-puppetlabs-sshkeys-core g10k > /dev/null && \
  \
 cd /infrastructure-puppet && \
+patch -u /usr/lib/ruby/vendor_ruby/octocatalog-diff/catalog-diff/filter/compilation_dir.rb bin/patches/octocatalogdiff_issue261_compilationdir.rb.patch &&
 git config --global --add safe.directory /infrastructure-puppet && \
 g10k -puppetfile -quiet && \
 echo && \
