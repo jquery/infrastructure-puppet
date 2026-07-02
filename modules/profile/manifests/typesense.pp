f84b4e5c6b84e6091e36a6f9361776d4045c04aa
Timo Tijhof <krinkle@fastmail.com>
2026-07-02 21:14:16 +0100

    search: Remove obsolete hieradata override for typesense version
    Ref https://github.com/jquery/infrastructure-puppet/issues/36

--- modules/profile/manifests/typesense.pp
+++ modules/profile/manifests/typesense.pp
@@ -2,7 +2,7 @@
 class profile::typesense (
   String[1] $tls_key_name = lookup('profile::typesense::tls_key_name'),
   String[1] $api_key = lookup('profile::typesense::api_key'),
-  String[1] $version = lookup('profile::typesense::version', {default_value => '0.24.0'}),
+  String[1] $version = lookup('profile::typesense::version', {default_value => '28.0'}),
 ) {
   class { 'typesense':
     api_key => $api_key,
