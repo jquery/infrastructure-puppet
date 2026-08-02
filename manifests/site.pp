0d7917d6a577b346f4c89e8212df7492b03e5ecf
Timo Tijhof <krinkle@fastmail.com>
2026-08-02 17:44:50 +0100

    site: remove search-02
    Ref https://github.com/jquery/infrastructure-puppet/issues/37

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,11 +42,6 @@ node 'puppet-04.ops.jquery.net' {
   role('puppet')
 }
 
-# 2 CPU, 2 GB mem, Debian 11 Bullseye
-node 'search-02.ops.jquery.net' {
-  role('search')
-}
-
 # 2 CPU, 2 GB mem, Debian 12 Bookworm
 node 'search-03.ops.jquery.net' {
   role('search')
