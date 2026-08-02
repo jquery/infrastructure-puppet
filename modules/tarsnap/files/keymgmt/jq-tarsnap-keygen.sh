853f0eed56f51a20d8b2f2b61f29d6e21a9a113b
Timo Tijhof <krinkle@fastmail.com>
2026-08-02 17:58:33 +0100

    tarsnap: silence stderr from `git var GIT_AUTHOR_IDENT` check
    Follows-up df66ec2b1a.
    
    Ref https://github.com/jquery/infrastructure-puppet/issues/91.

--- modules/tarsnap/files/keymgmt/jq-tarsnap-keygen.sh
+++ modules/tarsnap/files/keymgmt/jq-tarsnap-keygen.sh
@@ -25,7 +25,7 @@ git -C "$TARSNAP_KEYS_BASE_PATH" add "$KEYFILE"
 # Git already infers a default author email (username@hostname), extend this author name as well
 # Avoid "fatal: empty ident name (for <example@puppet-00.test>) not allowed"
 if [ "$(id -un)" != "root" ]; then
-  if ! git var GIT_AUTHOR_IDENT; then
+  if ! git var GIT_AUTHOR_IDENT 2>/dev/null; then
     export GIT_AUTHOR_NAME="$(id -un)"
     export GIT_COMMITTER_NAME="$(id -un)"
   fi
