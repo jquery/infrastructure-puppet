4deae91cc6b2954a43343c6e7563467e013c8c57
Timo Tijhof <krinkle@fastmail.com>
2026-08-10 16:06:04 +0100

    puppet: Tolerate absent gitconfig in jq-decom-instance.sh
    Applies df66ec2b1a, b908c2027f, and 853f0eed56 from jq-tarsnap-keygen.sh.

--- modules/profile/files/puppet/server/jq-decom-instance.sh
+++ modules/profile/files/puppet/server/jq-decom-instance.sh
@@ -11,6 +11,15 @@ TARSNAP_KEYFILE="$TARSNAP_KEYS_BASE_PATH/$INSTANCE.key"
 echo "=== Cleaning up $INSTANCE"
 
 if [ -f "$TARSNAP_KEYFILE" ]; then
+  # Git already infers a default author email (username@hostname), extend this author name as well
+  # Avoid "fatal: empty ident name (for <example@puppet-00.test>) not allowed"
+  if [ "$(id -un)" != "root" ]; then
+    if ! git var GIT_AUTHOR_IDENT 2>/dev/null; then
+      export GIT_AUTHOR_NAME="$(id -un)"
+      export GIT_COMMITTER_NAME="$(id -un)"
+    fi
+  fi
+
   echo "Cleaning up old Tarsnap backups and key file"
   if ! git -C "$TARSNAP_KEYS_BASE_PATH" diff-index --quiet HEAD --; then
     echo "The private git directory has uncommitted changes, please fix that before running this script."
