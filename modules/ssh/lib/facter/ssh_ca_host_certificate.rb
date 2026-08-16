f2947a31f0b1c1324e0d1ec2bc88f37cd4a80c35
Taavi Väänänen <hi@taavi.wtf>
2026-08-16 13:52:06 +0300

    ssh: Migrate custom facts to use net/ssh gem

--- modules/ssh/lib/facter/ssh_ca_host_certificate.rb
+++ modules/ssh/lib/facter/ssh_ca_host_certificate.rb
@@ -5,44 +5,29 @@ require 'time'
 
 Facter.add(:ssh_ca_host_certificate) do
   confine do
-    !Dir.glob('/etc/ssh/*-cert.pub').empty? && Facter::Core::Execution.which('ssh-keygen')
+    !Dir.glob('/etc/ssh/*-cert.pub').empty? && Gem::Specification.find_all_by_name('net-ssh').any?
   end
 
   setcode do
     hash = {}
 
+    require 'net/ssh'
+
     Dir.glob('/etc/ssh/*-cert.pub').each do |file_path|
-      cert_data = {}
-      cert_data['principals'] = []
+      ssh_data = Net::SSH::KeyFactory.load_public_key(file_path)
 
-      in_principals = false
-
-      # TODO: once on Bookworm and on the Debian provided packages,
-      # use the net/ssh gem based fact from the Wikimedia repo
-      ssh_data = Facter::Core::Execution.execute("ssh-keygen -L -f #{file_path}")
-      ssh_data.each_line do |line|
-        line.strip!
-
-        if in_principals
-          # this is the first non-principal line with the ssh version in bullseye
-          if line.include?('Critical Options:')
-            in_principals = false
-          else
-            cert_data['principals'] << line
-          end
-
-          next
-        end
-
-        if line.start_with?('Valid:') && !line.include?('forever')
-          parts = line.split(' ')
-          cert_data['lifetime_remaining_seconds'] = Time.parse(parts[4]).to_i - Time.now.to_i
-        elsif line.start_with?('Principals:')
-          in_principals = true
-        end
-      end
-
-      hash[file_path] = cert_data
+      # The OpenSSH certificate format interprets a valid_before time of 2^64-1
+      # (max for unsigned long) to mean "forever", but Puppet's max integer
+      # value is 9,223,372,036,854,775,807. Ensure valid_before_seconds is
+      # equal or less than Puppet's max value. Otherwise puppetserver's JRuby
+      # throws an error when accessing the facts hash:
+      #    https://github.com/net-ssh/net-ssh/pull/746
+      #    https://www.puppet.com/docs/puppet/7/lang_data_number.html#lang_data_number_integer_type
+      valid_before_seconds = [ssh_data.valid_before.to_i, 9_223_372_036_854_775_807].min
+      hash[file_path] = {
+        principals: ssh_data.valid_principals,
+        lifetime_remaining_seconds: valid_before_seconds - Time.now.to_i,
+      }
     end
 
     hash
