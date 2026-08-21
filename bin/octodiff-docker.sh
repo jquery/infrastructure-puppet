b8050853c60fdfaa0413a9faf466052e735639f2
Timo Tijhof <krinkle@fastmail.com>
2026-08-20 18:54:33 -0700

    bin: Upgrade octodiff-docker to Debian 13 Trixie
    Install octocatalog-diff from RubyGems instead.
    
    It appears to be compatible with Debian 13 Trixie and the newer
    versions of various packages it comes with such as Ruby (3.1 -> 3.3)
    and Puppet (7 -> 8).
    
    Ref https://github.com/jquery/infrastructure-puppet/issues/111

--- bin/octodiff-docker.sh
+++ bin/octodiff-docker.sh
@@ -3,13 +3,26 @@ set -euo pipefail
 
 repo_dir="$(realpath "$(dirname "$0")/..")"
 
+# ruby-dev:
+# Required for `gem install octocatalog-diff`
+#
+# cmake, pkg-config, libgit2-dev:
+# Documented requirements of "rugged", a dependency of octocatalog-diff.
+#
+# libzstd-dev:
+# Undocumented requiremenents of "rugged". https://github.com/libgit2/rugged/issues/990
+#
+# Pin exact octocatalog-diff version to easily apply the patch
+# from https://github.com/github/octocatalog-diff/issues/261
 script=" \
 echo 'Launching debian container...' && \
 echo 'Installing packages...' && \
 apt-get update -qq && \
-DEBIAN_FRONTEND=noninteractive DEBCONF_NOWARNINGS=yes apt-get install -y -qq git octocatalog-diff puppet-agent g10k > /dev/null && \
+DEBIAN_FRONTEND=noninteractive DEBCONF_NOWARNINGS=yes apt-get install -y -qq git puppet-agent g10k ruby-dev cmake pkg-config libgit2-dev libzstd-dev > /dev/null && \
+echo 'Installing octocatalog-diff...' && \
+gem install -q --silent octocatalog-diff -v 2.3.1 && \
 cd /infrastructure-puppet && \
-patch -u /usr/lib/ruby/vendor_ruby/octocatalog-diff/catalog-diff/filter/compilation_dir.rb bin/patches/octocatalogdiff_issue261_compilationdir.rb.patch && \
+patch -u \"\$(gem environment gemdir)/gems/octocatalog-diff-2.3.1/lib/octocatalog-diff/catalog-diff/filter/compilation_dir.rb\" bin/patches/octocatalogdiff_issue261_compilationdir.rb.patch && \
 git config --global --add safe.directory /infrastructure-puppet && \
 g10k -puppetfile -quiet -cachedir=/infrastructure-puppet/vendor_modules/.g10kcache && \
 echo && \
@@ -31,4 +44,4 @@ mkdir -p "$repo_dir/vendor_modules/.g10kcache"
 docker run --rm --interactive --tty \
   --mount type=bind,source="$repo_dir",target="/infrastructure-puppet",readonly \
   --mount type=bind,source="$repo_dir/vendor_modules",target="/infrastructure-puppet/vendor_modules" \
-  --entrypoint bash debian:bookworm-slim -c "$script"
+  --entrypoint bash debian:trixie-slim -c "$script"
