jquery/infrastructure-puppet

commit e61218a46d596a8a861cc52acd9ba54f1c79e2b2

wordpress: Add option to deny direct traffic to origin
I intially wrote a Facter fact on the puppetserver (backed by /etc/)
so that the value would be fetched only once, stored centrally,
and cached/reused/preserved/protected from external failure. This got
more complicated than I'd like, and I couldn't find a satisfactory
separation of concerns between wordpress and puppetserver profiles
such that the Facter file wouldn't be evaluated on other servers.

I then provisioning a file like /etc/cloudflare_ips.txt on each wp
host (there's only 3) with an "exec" using curl to a tmp file and
then atomically replace (mv -f) the etc file on success, so that in
case of HTTP failure, the last known data remains on the host, and we
can compile the nginx config files with that. The nginx ERB template
would read it with `scope.call_function('file', ..)` .

In the end I removed the whole persistent file and went straight
from a Puppet function to ERB. We still achieve the desired outcome
of failing with last known data, by raising an error during Puppet
compilation if the HTTP fetch fails. This means it can't generate
new file contents and gracefully keeps the server running with the
last known data and last generated server config. This has the added
benefit of failing in a way that's loud and clear in puppet-agent
output and syslog/journalctl, without taking the service down or
failing closed without any allowed IPs.

Ref https://github.com/jquery/infrastructure/issues/570

modules/profile/manifests/wordpress/blogs.pp
@@ -1,10 +1,11 @@
 # @summary various blog sites
 class profile::wordpress::blogs (
-  Profile::Wordpress::Blogs::Sites $sites             = lookup('profile::wordpress::blogs::sites'),
-  Optional[String[1]]              $wordpress_version = lookup('profile::wordpress::blogs::wordpress_version'),
-  String[1]                        $db_password_seed  = lookup('profile::wordpress::blogs::db_password_seed'),
-  Stdlib::Email                    $admin_email       = lookup('profile::wordpress::blogs::admin_email'),
-  String[1]                        $admin_password    = lookup('profile::wordpress::blogs::admin_password'),
+  Profile::Wordpress::Blogs::Sites $sites               = lookup('profile::wordpress::blogs::sites'),
+  Optional[String[1]]              $wordpress_version   = lookup('profile::wordpress::blogs::wordpress_version'),
+  String[1]                        $db_password_seed    = lookup('profile::wordpress::blogs::db_password_seed'),
+  Stdlib::Email                    $admin_email         = lookup('profile::wordpress::blogs::admin_email'),
+  String[1]                        $admin_password      = lookup('profile::wordpress::blogs::admin_password'),
+  Boolean                          $deny_external_traffic = lookup('profile::wordpress::blogs::deny_external_traffic', {default_value => false}),
 ) {
   include profile::wordpress::base
 
@@ -22,6 +23,12 @@ class profile::wordpress::blogs (
     local_path        => '/srv/wordpress/jquery-wp-content',
     local_user        => 'www-data',
     require           => Git::Clone['jquery-wp-content'],
+  }
+
+  if $deny_external_traffic {
+    $allow_only_ips = jqlib::cloudflare_ips()
+  } else {
+    $allow_only_ips = undef
   }
 
   $sites.each |String[1] $name, Hash $site| {
@@ -42,6 +49,7 @@ class profile::wordpress::blogs (
       db_password_seed => $db_password_seed,
       admin_email      => $admin_email,
       admin_password   => $admin_password,
+      allow_only_ips   => $allow_only_ips,
       config_files     => [
         "${dir}/jquery-config.php",
       ],

modules/profile/manifests/wordpress/docs.pp
@@ -9,6 +9,7 @@ class profile::wordpress::docs (
   String[1]            $wp_content_branch     = lookup('profile::wordpress::docs::wp_content_branch', {default_value => 'main'}),
   String               $append_title          = lookup('profile::wordpress::docs::append_title', {default_value => ''}),
   Boolean              $robots_txt_deny_all   = lookup('profile::wordpress::docs::robots_txt_deny_all', {default_value => false}),
+  Boolean              $deny_external_traffic = lookup('profile::wordpress::docs::deny_external_traffic', {default_value => false}),
   Boolean              $enable_object_cache   = lookup('profile::wordpress::docs::enable_object_cache', {default_value => false}),
   String[1]            $builder_password_seed = lookup('docs_builder_password_seed'),
   String               $prepend_host          = lookup('docs_prepend_host', {default_value => ''}),
@@ -29,6 +30,15 @@ class profile::wordpress::docs (
     local_path        => '/srv/wordpress/jquery-wp-content',
     local_user        => 'www-data',
     require           => Git::Clone['jquery-wp-content'],
+  }
+
+  if $deny_external_traffic {
+    # Always requests from within our cluster, especially builder-XX nodes.
+    $allow_only_ips =
+      jqlib::resource_hosts('class', 'profile::puppet::agent', true).jqlib::pdb_hosts2ips()
+      + jqlib::cloudflare_ips()
+  } else {
+    $allow_only_ips = undef
   }
 
   $sites.each |String[1] $name, Profile::Docs::Site $site| {
@@ -94,6 +104,7 @@ class profile::wordpress::docs (
       permalink_structure => '/%postname%/',
       gilded_wordpress    => true,
       robots_txt_deny_all => $robots_txt_deny_all,
+      allow_only_ips      => $allow_only_ips,
       config_files        => [
         "${dir}/jquery-config.php",
       ],

bin/octodiff-docker.sh
@@ -14,11 +14,15 @@ repo_dir="$(realpath "$(dirname "$0")/..")"
 #
 # Pin exact octocatalog-diff version to easily apply the patch
 # from https://github.com/github/octocatalog-diff/issues/261
+#
+# curl:
+# Required for jqlib::cloudflare_ips()
+#
 script=" \
 echo 'Launching debian container...' && \
 echo 'Installing packages...' && \
 apt-get update -qq && \
-DEBIAN_FRONTEND=noninteractive DEBCONF_NOWARNINGS=yes apt-get install -y -qq git puppet-agent g10k ruby-dev cmake pkg-config libgit2-dev libzstd-dev > /dev/null && \
+DEBIAN_FRONTEND=noninteractive DEBCONF_NOWARNINGS=yes apt-get install -y -qq git puppet-agent g10k ruby-dev cmake pkg-config libgit2-dev libzstd-dev curl > /dev/null && \
 echo 'Installing octocatalog-diff...' && \
 gem install -q --silent octocatalog-diff -v 2.3.1 && \
 cd /infrastructure-puppet && \

hieradata/environments/staging/roles/docs/wordpress.yaml
@@ -5,6 +5,7 @@
 # - /hierdata/environments/staging/roles/docs/wordpress.yaml [THIS FILE]
 
 profile::wordpress::docs::robots_txt_deny_all: true
+profile::wordpress::docs::deny_external_traffic: true
 profile::wordpress::docs::wordpress_version: ~
 
 profile::tarsnap::enabled: false

modules/jqlib/lib/puppet/functions/jqlib/cloudflare_ips.rb
@@ -1,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'ipaddr'
+
+Puppet::Functions.create_function(:'jqlib::cloudflare_ips') do
+  dispatch :main do
+    return_type 'Array[Stdlib::IP::Address]'
+  end
+
+  def main
+    # Use a global variable as cache to de-duplicate any repeat calls
+    @cloudflare_ips ||= begin
+      # https://www.cloudflare.com/ips/
+      urls = [
+        'https://www.cloudflare.com/ips-v4/',
+        'https://www.cloudflare.com/ips-v6/',
+      ]
+      ranges = urls.flat_map do |url|
+        output = Facter::Core::Execution.execute(
+          "/usr/bin/curl --fail --silent --show-error --location --retry 3 #{url}",
+          on_fail: nil
+        )
+        raise Puppet::Error, "Failed to fetch IPs from #{url}" if output.nil?
+        output.lines.map(&:strip).reject(&:empty?)
+      end.uniq
+
+      ranges.each do |range|
+        begin
+          IPAddr.new(range)
+        rescue ArgumentError
+          raise Puppet::Error, "Invalid IP range: #{range}"
+        end
+      end
+      raise Puppet::Error, 'List of IPs must be non-empty' if ranges.empty?
+
+      ranges
+    end
+  end
+end

modules/wordpress/manifests/site.pp
@@ -21,6 +21,9 @@ define wordpress::site (
   String[1]                 $permalink_structure = '/%year%/%monthnum%/%day%/%postname%/',
   Boolean                   $robots_txt_deny_all = false,
   Boolean                   $gilded_wordpress    = false,
+  Optional[
+    Array[Stdlib::IP::Address]
+  ]                         $allow_only_ips      = undef,
   Stdlib::Unixpath          $webroot             = $base_path,
 ) {
   mariadb::database { "wordpress_${title}": }

modules/wordpress/templates/site/site.nginx.erb
@@ -24,6 +24,15 @@ server {
   log_not_found off;
   server_tokens off;
 
+<%- if @allow_only_ips -%>
+<% raise 'allow_only_ips must be non-empty' unless !@allow_only_ips.empty? %>
+  allow 127.0.0.1;
+<% @allow_only_ips.each do |ip| %>
+  allow <%= ip %>;
+<% end %>
+  deny all;
+<%- end -%>
+
   location = /favicon.ico {
     access_log off;
     try_files /wp-content/themes/<%= @active_theme %>/i/favicon.ico /wp-content/themes/jquery/i/favicon.ico;

commit 42f0ae38888e719b2676b57635266be8a84f3744

wordpress: Add fastly_ips to wordpress::docs allow_only_ips
Follows-up 383ec1c0f5 and e61218a46d, which broke https://releases.jquery.com.

modules/jqlib/lib/puppet/functions/jqlib/fastly_ips.rb
@@ -1,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'ipaddr'
+
+Puppet::Functions.create_function(:'jqlib::fastly_ips') do
+  dispatch :main do
+    return_type 'Array[Stdlib::IP::Address]'
+  end
+
+  def main
+    # Use a global variable as cache to de-duplicate any repeat calls
+    @fastly_ips ||= begin
+      # https://www.fastly.com/documentation/reference/api/utils/public-ip-list/
+      output = Puppet::Util::Execution.execute(
+        ['/usr/bin/curl', '--fail', '--silent', '--show-error', '--location', '--retry', '3', 'https://api.fastly.com/public-ip-list'],
+        failonfail: true
+      )
+      data = JSON.parse(output)
+      ranges = [
+        *data['addresses'],
+        *data['ipv6_addresses']
+      ]
+
+      ranges.each do |range|
+        begin
+          IPAddr.new(range)
+        rescue ArgumentError
+          raise Puppet::Error, "Invalid IP range: #{range}"
+        end
+      end
+      raise Puppet::Error, 'List of IPs must be non-empty' if ranges.empty?
+
+      ranges
+    end
+  end
+end

modules/profile/manifests/wordpress/blogs.pp
@@ -26,7 +26,10 @@ class profile::wordpress::blogs (
   }
 
   if $deny_external_traffic {
-    $allow_only_ips = jqlib::cloudflare_ips()
+    $allow_only_ips = (
+      jqlib::resource_hosts('class', 'profile::puppet::agent').jqlib::pdb_hosts2ips()
+      + jqlib::fastly_ips()
+      + jqlib::cloudflare_ips()
   } else {
     $allow_only_ips = undef
   }

modules/profile/manifests/wordpress/docs.pp
@@ -33,9 +33,12 @@ class profile::wordpress::docs (
   }
 
   if $deny_external_traffic {
-    # Always allow requests from within our cluster, especially builder-XX nodes.
     $allow_only_ips = (
+      # Always allow requests from within our cluster, especially builder-XX nodes.
       jqlib::resource_hosts('class', 'profile::puppet::agent').jqlib::pdb_hosts2ips()
+      # releases.jquery.com
+      + jqlib::fastly_ips()
+      # jquery.com, api.jquery.com, etc.
       + jqlib::cloudflare_ips()
     )
   } else {